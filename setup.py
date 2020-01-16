90ead22f77ddd1320b2571874806920549b4b854
brodderickrodriguez <bcr@brodderick.com>
2020-01-15 19:38:58 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(name='hypertune',
       version='0.1.0',
       description='A package to tune ML hyperparameters using Particle Swarm Optimization.',
-      url='https://github.com/brodderickrodriguez/pso_hyper_tune',
+      url='https://github.com/brodderickrodriguez/hypertune',
       author='Brodderick Rodriguez',
       author_email='bcr@brodderick.com',
       license='Apache-2.0',
